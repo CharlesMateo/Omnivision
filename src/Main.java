9055c854157c33c0f439741a38e7e59568e1198f
CharlesMateo <Charlesmateo18@gmail.com>
2024-09-24 13:57:31 -0700

    Semi-First Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello User...");
+
+        String fullname = "Charles Mateo";
+
+        System.out.println(fullname);
+    }
+}
